ac9e4dddeffeae2efd9b381070c1be95eab5e6a0
Le Trung Hai <hozymary@gmail.com>
2022-10-08 22:26:29 +0800

    Refactor the screens with aggregate module

---
+++ src/screens/index.js
@@ -0,0 +1,4 @@
+export { default as HomePage } from "./HomePage";
+export { default as QuitPage } from "./QuitPage";
+export { default as SuccessPage } from "./SuccessPage";
+export { default as TimerPage } from "./TimerPage";
