6a448ff131ad2bf44d67c5b6eaa9e9d443ae5c7d
Le Trung Hai <hozymary@gmail.com>
2022-10-11 10:57:41 +0800

    Add wrapper around app to prevent background flashing
    Before, moving between screens created a flash of white background, so
    we wrap the app with a background color matching that of the screens.

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 import { HomePage, FailPage, SuccessPage, TimerPage } from "./src/screens";
-import { ThemeProvider } from "./src/helpers";
+import { createStyles, ThemeProvider } from "./src/helpers";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const styles = useStyles();
+
   return (
     <ThemeProvider>
       <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="HomePage" component={HomePage} />
-            <Stack.Screen name="TimerPage" component={TimerPage} />
-            <Stack.Screen name="FailPage" component={FailPage} />
-            <Stack.Screen name="SuccessPage" component={SuccessPage} />
-          </Stack.Navigator>
-        </NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="HomePage" component={HomePage} />
+              <Stack.Screen name="TimerPage" component={TimerPage} />
+              <Stack.Screen name="FailPage" component={FailPage} />
+              <Stack.Screen name="SuccessPage" component={SuccessPage} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </SafeAreaView>
       </SafeAreaProvider>
     </ThemeProvider>
   );
 }
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    flex: 1,
+    backgroundColor: theme.primaryColor,
+  },
+}));
