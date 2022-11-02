54b37f9cf042f7ce3c7c2e129c39b7dbdfb95368
Le Trung Hai <hozymary@gmail.com>
2022-11-02 18:26:11 +0800

    Style AboutPage and LogPage

--- src/screens/settings/LogPage.tsx
+++ src/screens/settings/LogPage.tsx
@@ -1,12 +1,35 @@
 import { Text } from "react-native";
 
+import { createStyles } from "../../helpers";
 import SettingsScreen from "./SettingsScreen";
 
 export default function LogPage({ navigation }) {
+  const styles = useStyles();
+
   return (
-    <SettingsScreen title="LogPage" onBack={() => navigation.navigate("Home")}>
-      <Text>You have started X focusing sessions today.</Text>
-      <Text>The last focus session ended at .</Text>
+    <SettingsScreen
+      title="Focusing Log"
+      onBack={() => navigation.navigate("Home")}
+    >
+      <Text style={styles.text}>
+        You have started <Text style={{ fontWeight: "700" }}>X</Text> focusing
+        sessions today.
+      </Text>
+      <Text style={styles.text}>
+        The last focus session ended at{" "}
+        <Text style={{ fontWeight: "700" }}>X</Text>.
+      </Text>
     </SettingsScreen>
   );
 }
+
+const useStyles = createStyles((theme) => ({
+  text: {
+    width: "80%",
+    marginTop: 60,
+    marginBottom: 8,
+    color: theme.textColor,
+    fontSize: theme.fontSizes.md,
+    textAlign: "center",
+  },
+}));
