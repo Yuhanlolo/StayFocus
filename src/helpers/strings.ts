f7e043f2738d77196923b4251575fcbbe29954a3
Yuhan Luo <yuhanluo@cityu.edu.hk>
2023-02-26 12:18:47 +0800

    Update strings.ts
    Added a few more reflection questions for randomization

--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -8,30 +8,34 @@ function getStrings({
   return {
     leaveFocusDialog: {
       dialogTitle: 'Quick questions before you leave',
-      numberOfQuestions: 2,
-      fixedQuestions: [['Why do you want to check your phone now?']],
-      randomizedQuestions: [
-        ["How do you feel without smartphone's distraction?"],
-        ['Any thoughts about focusing for a longer duration next time?'],
-        [
-          'Lorem ipsum morbi molestie augue tincidunt purus ultrices',
-          'A wealth of information creates a poverty of attention',
-        ],
-        [`Placeholder for ${focusDurationMinutes}`],
-      ],
-      finalMessage: 'Your session has ended.',
-    },
-    focusEndedDialog: {
-      dialogTitle: 'Quick reflection questions',
-      initialMessage: `Your focus session has ended. You focused on ${plan} for ${completedMinutes}.`,
       numberOfQuestions: 3,
       fixedQuestions: [['Why do you want to check your phone now?']],
       randomizedQuestions: [
-        ["How do you feel without smartphone's distraction?"],
-        ['Any thoughts about focusing for a longer duration next time?'],
+        [`You have been focused for ${focusDurationMinutes}, are you sure to leave?`],
+        ['Is anything distracting you at this moment?'],
+        ['Is anything going on in your mind now?'],
+        ['What is your plan after leaving this focus session?'],
         [
-          'Lorem ipsum morbi molestie augue tincidunt purus ultrices',
-          'A wealth of information creates a poverty of attention',
+          'What was your original plan for this focus session?',
+          'Great Plan! How did it go?',
+        ],
+        [`Placeholder for ${focusDurationMinutes}`],
+      ],
+      finalMessage: 'Thank you. Your focus session has ended.',
+    },
+    focusEndedDialog: {
+      dialogTitle: 'Quick reflection questions',
+      initialMessage: `Your focus session has ended because you have left the app. You have been focused for ${completedMinutes}.`,
+      numberOfQuestions: 3,
+      fixedQuestions: [['Why did you just check your phone?']],
+      randomizedQuestions: [
+        [`You have been focused for ${completedMinutes}, are you sure to leave?`],
+        ['Was anything distracting you earlier?'],
+        ['Was anything going on in your mind earlier ?'],
+        ['What was your plan after leaving this focus session?'],
+        [
+           'What was your original plan for this focus session?',
+           'Great Plan! How did it go?',
         ],
         [`Placeholder for ${focusDurationMinutes}`],
       ],
@@ -43,20 +47,16 @@ function getStrings({
       numberOfQuestions: 3,
       fixedQuestions: [
         [
-          `How is your productivity during the past ${completedMinutes} minutes?`,
+          `How did the focus session go?`,
         ],
       ],
       randomizedQuestions: [
-        ["How do you feel without smartphone's distraction?"],
-        ['Any thoughts about focusing for a longer duration next time?'],
-        ['A wealth of information creates a poverty of attention'],
-        ['If they killed him tonight, at least he would die alive.'],
-        [
-          'Give every man thy ear, but few thy voice;',
-          "Take each man's censure, but reserve thy judgment.",
-        ],
+        ['How do you feel without the distraction from your phone?'],
+        ['How did you managed to complete the session without being distracted by your phone?'],
+        ['What the next plan?'],
+        [`How is your productivity during the past ${completedMinutes} minutes?`],
       ],
-      finalMessage: "Great! You've done this focusing session.",
+      finalMessage: "Great! You've completed this focusing session.",
     },
   };
 }
