a9950520c4916cc9ec50f0ae3ad9895e080b9d11
Le Trung Hai <hozymary@gmail.com>
2023-02-28 21:17:44 +0800

    Fix counting unfinished sessions

--- src/api/analytics.ts
+++ src/api/analytics.ts
@@ -5,20 +5,22 @@ function _AnalyticsData(date: string): AnalyticsData {
   const sessions = getAppStore().focusSessions;
   console.log(sessions);
   console.log(date);
-  let count = 0;
+  let completedCount = 0;
   let minutes = 0;
   let lastSessionEndTime = '';
   for (const s of sessions) {
-    console.log(s);
     if (s.startTime.startsWith(date)) {
-      count++;
+      const l = s.giveUpAttempts.length;
+      if (l === 0 || !s.giveUpAttempts[l - 1].givenUp) {
+        completedCount++;
+      }
       minutes = minutes + s.completedMinutes;
       lastSessionEndTime = s.endTime;
     }
   }
 
   return {
-    sessionsCount: count,
+    sessionsCount: completedCount,
     sessionsMinutes: minutes,
     lastSessionEndTime: lastSessionEndTime,
   };
