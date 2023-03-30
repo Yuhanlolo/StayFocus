80869e24cc6f8c83bcc73a8cff209f7031624336
Le Trung Hai <hozymary@gmail.com>
2023-03-30 22:36:35 +0800

    Fix crashes on launch and LogPage

--- android/app/src/main/java/com/stayfocus/MainActivity.java
+++ android/app/src/main/java/com/stayfocus/MainActivity.java
@@ -52,9 +52,9 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+    super.onCreate(savedInstanceState);
 
-    if (UsageStatsModule.getUsageStatsList(this).isEmpty()) {
+    if (!UsageStatsModule.hasUsagePermission(this)) {
       Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
       startActivity(intent);
     }
