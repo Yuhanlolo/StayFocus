55a6ae77334cca7fc98949b12a126e9dd4fe345e
Yuhan Luo <yuhanluo@cityu.edu.hk>
2023-03-31 13:55:35 +0800

    Update chatScript.js

--- src/chat_reflection_scripts/chatScript.js
+++ src/chat_reflection_scripts/chatScript.js
@@ -1,7 +1,7 @@
 const chatScript = {
   home: "Ready to start a focus session with me?",
-  set: "How long would you like to be focused?",
-  quit: "Are you sure to leave the focus mode now?",
+  set: "Please select or enter a focus duration.",
+  quit: "Do you still want to stay in the focus mode",
   congrat1: "Great job! You have completed the focus mode for X minutes!",
   congrat2: "Do you want to take a break or continue to stay focused?",
 }
