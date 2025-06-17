b11b7c6b32351a2ba42c4bab2921b73d09953b14
Not Liang Minhui <108807634+MinhuiLIANG@users.noreply.github.com>
2025-06-17 14:29:32 +0800

    Update firebase.ts

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBB-3guAkCcbaUtIWgUA4LI6jqvTqFsfC0",
-  authDomain: "stayfocus-cityu.firebaseapp.com",
-  projectId: "stayfocus-cityu",
-  storageBucket: "stayfocus-cityu.appspot.com",
-  messagingSenderId: "253056798296",
-  appId: "1:253056798296:web:607e8084588a70281b042d",
+  apiKey: 'your-api-key',
+  authDomain: 'your-auth-domain',
+  projectId: 'your-project-id',
+  storageBucket: 'your-storage',
+  messagingSenderId: 'your-id',
+  appId: 'your-id',
 };
 
 export const app = initializeApp(firebaseConfig);
