549096b448ca8b5e10b7179546ac72321eb8d4c1
Not Liang Minhui <108807634+MinhuiLIANG@users.noreply.github.com>
2025-06-17 14:30:21 +0800

    Update firebase.ts

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,13 +1,13 @@
 import {initializeApp} from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBYTP-4HS5FCz0jld18Eg4E7brm-FX5XBM',
-  authDomain: 'stayfocused-76151.firebaseapp.com',
-  projectId: 'stayfocused-76151',
-  storageBucket: 'stayfocused-76151.appspot.com',
-  messagingSenderId: '739238127090',
-  appId: '1:739238127090:web:363043451cfc12a18fb76b',
-  measurementId: 'G-0NP557SPB1',
+  apiKey: 'your-api-key',
+  authDomain: 'your-auth-domain',
+  projectId: 'your-project-id',
+  storageBucket: 'your-storage',
+  messagingSenderId: 'your-id',
+  appId: 'your-id',
+  measurementId: 'your-id',
 };
 
 export const app = initializeApp(firebaseConfig);
